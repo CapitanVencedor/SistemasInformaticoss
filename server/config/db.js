2ad8185249e96ebaa71d4fa2f267d1a86aad6f77
welin silva <nene__1992@hotmail.com>
2025-05-15 16:29:27 +0200

    ip arriba1

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'ErwnizEv5',
+  password: 'EwnizEv5',
   database: 'aurum_db',
   waitForConnections: true,
   connectionLimit: 10
